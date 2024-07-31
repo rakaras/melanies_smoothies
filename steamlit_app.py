a631c864a9c0838f80da4e5967e60ad2073a1503
rakaras <ron.karas@gmail.com>
2024-07-31 17:28:30 -0400

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -16,7 +16,7 @@ from snowflake.snowpark.functions import col
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME').col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
