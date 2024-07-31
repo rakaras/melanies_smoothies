48b96ec45b94c76c709baaac7e4070c9a01bddf8
rakaras <ron.karas@gmail.com>
2024-07-31 17:35:14 -0400

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -18,8 +18,8 @@ session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 # st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect(
@@ -34,6 +34,9 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df(loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].loc[0]
+        st.write('The search value for ' +fruit_chosen + ' is ' +search_on)
         st.subheader=(fruit_chosen + ' Nutrition information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
