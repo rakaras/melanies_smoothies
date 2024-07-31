39a6a96d77c46143c752526b93f3c28ecfbbb43f
rakaras <ron.karas@gmail.com>
2024-07-31 16:52:35 -0400

    Update steamlit_app.py
    fruitvise added

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -38,3 +39,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered ' + name_on_order +'!' , icon="✅")
+
+# new section
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
