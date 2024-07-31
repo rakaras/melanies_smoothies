03ad7fc5e20b31f3a51fd6830bdff38479d8f5cf
rakaras <ron.karas@gmail.com>
2024-07-31 17:07:27 -0400

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -30,6 +30,9 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader=(fruit_chosen + ' Nutrition information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
@@ -40,7 +43,4 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered ' + name_on_order +'!' , icon="✅")
 
-# new section
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response)
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
